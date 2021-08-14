697211316e746dacd2810a24cd9c3bd903ebc95c
Nvard Asatryan <nvard.asatryan96@gmail.com>
2021-08-15 01:39:49 +0400

    sdasd

--- js/script.js
+++ js/script.js
@@ -2,16 +2,29 @@ let temp = true;
 $('.burger').click(function(){
     
     temp = !temp;
-    $('.mob-menu').toggle(300);
+   // $('.mob-menu').toggle();
     if(!temp){
         $('#burg').removeClass('fa-bars');
         $('#burg').addClass('fa-close');
+        $('.inner-header').addClass('cross');
+        $('.mob-menu').addClass('active-list');
 
     } else{
         $('#burg').addClass('fa-bars');
         $('#burg').removeClass('fa-close');
+        $('.inner-header').removeClass('cross');
+        $('.mob-menu').removeClass('active-list');
+
+
     }
 });
+$('.menu-link').click(function(){
+  $('.mob-menu').removeClass('active-list');
+  $('.inner-header').removeClass('cross');
+  $('#burg').removeClass('fa-close');
+  $('#burg').addClass('fa-bars');
+
+})
 var swiper = new Swiper(".mySwiper", {
     loop: true,
     loopFillGroupWithBlank: true,
@@ -44,6 +57,12 @@ var swiper = new Swiper(".mySwiper", {
   //link animate
   $(document).ready(function() {
     $("a.menu-link").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 700);
+    });
+  });
+  $(document).ready(function() {
+    $(".get").click(function(event) {
         event.preventDefault();
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 700);
     });
